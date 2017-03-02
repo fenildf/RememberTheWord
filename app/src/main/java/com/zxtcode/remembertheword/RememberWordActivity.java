9b9741d12e6c6b17b5c7162e5317b421f19096fb
Tarent <lan4627@gmail.com>
2017-03-02 08:35:15 +0800

    添加通知

---
+++ app/src/main/java/com/zxtcode/remembertheword/RememberWordActivity.java
@@ -0,0 +1,17 @@
+package com.zxtcode.remembertheword;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+
+/**
+ * Created by tarent on 2017/3/1.
+ */
+
+public class RememberWordActivity extends WearableActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
